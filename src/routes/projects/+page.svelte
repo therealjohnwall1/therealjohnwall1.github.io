b46efd7ab31939dbf1309c4e8f4fe5059c9dfa8f
Andy Van <108160452+therealjohnwall1@users.noreply.github.com>
2026-08-03 12:19:18 -0700

    Update projects page with project details
    Added project descriptions for various projects including Anchor, Active Recoil Compensation, Odyssey, and terrAIn.

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,5 +3,62 @@
 
 <h1>Projects</h1>
 
-<p>This page is under construction. Please check back later for updates on my projects.</p>
-<p> i swear i have them chill </p>
+<p>
+    <i>
+    TODO: Link github repos after cleaning code
+    </i>
+</p>
+
+<hr />
+
+<div class="Project">
+    <div style="display: flex; align-items: flex-start; gap: 1rem;">
+        <div style="flex: 1;">
+            <p>
+                <b>Anchor</b><br />
+                <i>Active noise canceling chip (TinyTapeout)</i><br />
+            </p>
+        </div>
+    </div>
+</div>
+
+<hr />
+
+<div class="Project">
+    <div style="display: flex; align-items: flex-start; gap: 1rem;">
+        <div style="flex: 1;">
+            <p>
+                <b>Active Recoil Compensation — Quadruped</b><br />
+                <i>Robot dog with a mounted launcher and active recoil compensation(In simulation)</i><br />
+            </p>
+        </div>
+    </div>
+</div>
+
+<hr />
+
+<div class="Project">
+    <div style="display: flex; align-items: flex-start; gap: 1rem;">
+        <div style="flex: 1;">
+            <p>
+                <b>Odyssey</b><br />
+                <i>Dead reckoning / GPS-denied navigation for UAVs</i><br />
+            </p>
+        </div>
+    </div>
+</div>
+
+<hr />
+
+<div class="Project">
+    <div style="display: flex; align-items: flex-start; gap: 1rem;">
+        <div style="flex: 1;">
+            <p>
+                <b>terrAIn</b><br />
+                <i>Distributed soil monitoring</i><br />
+                Multiple soil sensor nodes (temperature, elevation, moisture,
+                pressure) for monitoring.<br />
+            </p>
+        </div>
+    </div>
+</div>
