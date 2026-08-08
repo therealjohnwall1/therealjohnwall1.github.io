9e06f776ee9f4e9c50b07497bbdd70c008e024e9
Andy Van <108160452+therealjohnwall1@users.noreply.github.com>
2026-08-07 18:35:15 -0700

    Update student year and interests description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <h1>Andy's Website</h1>
 
-<p> Third year computer science student from San Jose State University. Below are my current interests emerging from coursework
+<p> Fourth year computer science and math student from San Jose State University. Below are my current interests emerging from coursework
 and work experience:
 </p>
 
